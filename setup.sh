adb6c98cf2f436d2a0099ae310fc612f5cf6c542
Long Vu <longvu727@yahoo.com>
2024-10-08 06:51:04 -0700

    add FootballSquaresDotNet repository to setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+git clone git@github.com:longvu727/FootballSquaresDotNet.git
 git clone https://github.com/longvu727/FootballSquaresLibs
 git clone git@github.com:longvu727/SquareMicroservices.git Microservices/SquareMicroservices/
 git clone git@github.com:longvu727/GameMicroservices.git Microservices/GameMicroservices/
