5dd08ea1e7bc835755e111bde386f925031d4bce
Long Vu <longvu727@yahoo.com>
2024-10-08 06:47:14 -0700

    FootballSquareDotNet init

---
+++ setup.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/longvu727/FootballSquaresLibs
+git clone git@github.com:longvu727/SquareMicroservices.git Microservices/SquareMicroservices/
+git clone git@github.com:longvu727/GameMicroservices.git Microservices/GameMicroservices/
+git clone git@github.com:longvu727/FootballSquareGameMicroservices.git Microservices/FootballSquareGameMicroservices/
+git clone git@github.com:longvu727/UserMicroservices.git Microservices/UserMicroservices/
